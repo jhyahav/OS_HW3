1d55a1e57ab85a09901e584fadefb88db6617c00
Jonathan Yahav <jonathanhuck@mail.tau.ac.il>
2023-05-23 00:12:50 +0300

    getters/setters, progress on write

--- message_slot.c
+++ message_slot.c
@@ -22,37 +22,35 @@ MODULE_LICENSE("GPL");
     Each Channel has an ID and a message.
 */
 
-typedef struct
+struct Slot
 {
     int minor;
     struct Slot *next_slot;
     uint32_t channel_count;
     struct Channel *channel_linked_list_head;
     struct Channel *current_channel;
-} Slot;
+};
 
-typedef struct
+struct Channel
 {
     int channel_id;
-    char message[BUF_LEN];
-    int message_length;
+    char message[BUF_LEN]; // TODO: turn this into a pointer w/o buffer length?
+    ssize_t message_length;
     struct Channel *next;
-} Channel;
+};
 
-static int dev_open_flag = 0;
+static struct Slot *slot_linked_list_head;
+static struct Slot *current_slot;
 
-static Slot device_info;
+// Structs are read & manipulated using getter/setter functions. See end of file.
 
 //================== DEVICE FUNCTIONS ===========================
 static int device_open(struct inode *inode,
                        struct file *file)
 {
-    if (dev_open_flag == 1)
-    {
-        return -EBUSY;
-    }
 
-    ++dev_open_flag;
+    unsigned int minor = iminor(inode);
+
     return SUCCESS;
 }
 
@@ -61,8 +59,6 @@ static int device_release(struct inode *inode,
                           struct file *file)
 {
     // TODO: implement
-    unsigned long flags;
-    --dev_open_flag;
     return SUCCESS;
 }
 
@@ -77,24 +73,62 @@ static ssize_t device_read(struct file *file,
 }
 
 //---------------------------------------------------------------
-// a processs which has already opened
-// the device file attempts to write to it
 static ssize_t device_write(struct file *file,
                             const char __user *buffer,
                             size_t length,
                             loff_t *offset)
 {
-    // TODO: implement
-    ssize_t i;
-    for (i = 0; i < length && i < BUF_LEN; ++i)
+
+    int validity = set_channel_and_check_validity(file, length);
+    if (validity != SUCCESS)
     {
-        // get_user(the_message[i], &buffer[i]);
-        // if (1 == encryption_flag)
-        //     the_message[i] += 1;
+        return validity;
     }
 
-    // return the number of input characters used
-    return i;
+    return write_from_buffer(buffer, length);
+}
+
+ssize_t write_from_buffer(const char __user *buffer, size_t length)
+{
+    reset_current_message();
+    char *message = get_current_message();
+    ssize_t num_bytes_written = 0;
+    for (; num_bytes_written < length; num_bytes_written++)
+    {
+        get_user(message[num_bytes_written], &buffer[num_bytes_written]);
+    }
+
+    set_current_message_length(num_bytes_written);
+
+    return num_bytes_written;
+}
+
+int set_channel_and_check_validity(struct file *file, size_t length)
+{
+    int channel_set = set_channel_from_file(file);
+    if (channel_set != SUCCESS)
+    {
+        return channel_set;
+    }
+
+    int valid_length = is_valid_length(length);
+    if (valid_length != SUCCESS)
+    {
+        return valid_length;
+    }
+    return SUCCESS;
+}
+
+// Returns SUCCESS if set successfully or -EINVAL if invalid
+int set_channel_from_file(struct file *file)
+{
+    // TODO: implement
+    return -EINVAL;
+}
+
+int is_valid_length(int length)
+{
+    return length > BUF_LEN || length < 1 ? -EMSGSIZE : SUCCESS;
 }
 
 //----------------------------------------------------------------
@@ -155,3 +189,75 @@ static void clean_up_memory(void)
 
 module_init(device_init);
 module_exit(device_cleanup);
+
+//================== GETTERS & SETTERS ===========================
+
+static struct Channel *get_current_channel(void)
+{
+    return current_slot->current_channel;
+}
+
+static void set_current_channel(struct Channel *channel)
+{
+    current_slot->current_channel = channel;
+}
+
+static u_int32_t get_channel_count(void)
+{
+    return current_slot->channel_count;
+}
+
+static void set_channel_count(u_int32_t count)
+{
+    current_slot->channel_count = count;
+}
+
+static char *get_current_message(void)
+{
+    return get_current_channel()->message;
+}
+
+static void allocate_current_message(size_t size)
+{
+    struct Channel *current_channel = get_current_channel();
+    current_channel->message = (char *)kmalloc()
+}
+
+static void *reset_current_message(void)
+{
+    char *current_message = get_current_message();
+    if (current_message != NULL)
+    {
+        kfree(current_message);
+    }
+}
+
+static int get_current_message_length(void)
+{
+    return get_current_channel()->message_length;
+}
+
+static void set_current_message_length(int length)
+{
+    get_current_channel()->message_length = length; // TODO: make sure I work!
+}
+
+static int get_current_channel_id(void)
+{
+    return get_current_channel()->channel_id;
+}
+
+static void set_current_channel_id(int id)
+{
+    get_current_channel()->channel_id = id;
+}
+
+static struct Channel *get_next_channel(void)
+{
+    return get_current_channel()->next;
+}
+
+static void set_next_channel(struct Channel *next_channel)
+{
+    get_current_channel()->next = next_channel;
+}
